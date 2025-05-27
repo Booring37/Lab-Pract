8b3d6c33a8fa3d70d982d1151843cd9c537a6e6c
Booring37 <69788858+Booring37@users.noreply.github.com>
2025-05-27 20:22:43 +0545

    Create interpolation2.py

---
+++ NM/Lab2/interpolation2.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def fact(num):
+    if num <= 0:
+        return 1
+    return num * fact(num - 1)
+
+x = 0.2 
+narr = np.zeros((5,6))
+narr[:,0] = [0,1,2,3,4]
+narr[:,1] = [8,-1,-4,5,32] 
+
+def delta():
+    for i in range(2,6):
+        for j in range(6 - i):
+            narr[j,i] = narr[j+1,i-1] - narr[j,i-1]
+
+delta()
+print("Difference Table:")
+print(narr)
+
+h = narr[1,0] - narr[0,0]
+p = (x - narr[0,0]) / h
+y = narr[0,1]
+
+p_term = 1
+for i in range(1, 5):
+    p_term *= (p - (i - 1))
+    y += (p_term * narr[0,i+1]) / fact(i)
+
+print("\nInterpolated value at x = 0.2 is:", y)
