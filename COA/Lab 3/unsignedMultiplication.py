87bcedddfd4432935a1ca8f822c065de41c966cb
Booring37 <69788858+Booring37@users.noreply.github.com>
2025-06-06 14:06:54 +0545

    Add files via upload

---
+++ COA/Lab 3/unsignedMultiplication.py
@@ -0,0 +1,27 @@
+C = 0
+A = 0
+mS = str(input("Enter Multiplicant: "))
+qS = str(input("Enter Multiplier: "))
+
+M = int(mS,2)
+Q = int(qS,2)
+count = len(mS) if M>Q else len(qS)
+n=count
+
+print(M, Q, count)
+
+while count > 0:
+    if (Q & 1 == 1):
+        temp = A + M
+        C = temp >> n
+        A = temp & ((1<<n)-1)
+    new_Q = (Q >> 1) | ((A & 1) << (n - 1))
+    new_A = (A >> 1) | (C << (n - 1))
+    A = new_A
+    Q = new_Q
+    C = 0
+    count -= 1
+    
+product = (A << n) | Q
+print("Product in decimal = ", product) 
+print("Product in binary = {:8b}".format(product))
